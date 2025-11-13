8aba713c9391caa79e8417970e6b8ebdcf4eff6e
Onur DÖKER <odoker@msn.com>
2025-11-14 00:22:23 +0300

    File Upload and Display

---
+++ Chapter A - Basic Integration Projects/Project04 - File Upload and Display/app.py
@@ -0,0 +1,13 @@
+"""
+Title: File Upload and Display
+
+Subject: Media management, form handling and security
+
+Components: Flask, Flask-SQLAlchemy,werkzeug.utils.secure_filename
+
+Requirements:
+- Define a model named Not (including name, path and upload_date)
+- Create a file upload form and retrieve the file using request.files
+- Save the file securely to the static/uploads directory using secure_filename()
+- Display a list of uploaded files and provide access to their static URLs
+"""
